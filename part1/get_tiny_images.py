47064ff9d364928347e60a0e90a9ff3c6210f508
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-04-09 17:50:08 +0800

    Update get_tiny_images.py
    1. Crop the maximum square from center before resizing.
    2. Change interpolation to INTER_AREA
    3. PEP8

--- part1/get_tiny_images.py
+++ part1/get_tiny_images.py
@@ -2,40 +2,39 @@ from PIL import Image
 import numpy as np
 import cv2
 
-def get_tiny_images(image_paths):
-    #############################################################################
-    # TODO:                                                                     #
-    # To build a tiny image feature, simply resize the original image to a very #
-    # small square resolution, e.g. 16x16. You can either resize the images to  #
-    # square while ignoring their aspect ratio or you can crop the center       #
-    # square portion out of each image. Making the tiny images zero mean and    #
-    # unit length (normalizing them) will increase performance modestly.        #
-    #############################################################################
+def crop_square(img):
+    h, w = img.shape
+    min_hw = min(h, w)
+    y = h // 2 - min_hw // 2; x = w // 2 - min_hw // 2
+    return img[y:y+min_hw, x:x+min_hw]
+
+def get_tiny_images(image_paths, interpolation=cv2.INTER_AREA):
+    ###########################################################################
+    # TODO:                                                                   #
+    # To build a tiny image feature, simply resize the original image to      # 
+    # a very small square resolution, e.g. 16x16.                             #
+    # You can either ignore their aspect ratio or crop the center square      #
+    # portion out of each image before resizing the images to square.         #
+    # Making the tiny images zero mean and unit length (normalizing them)     # 
+    # will increase performance modestly.                                     #  
+    ###########################################################################
     '''
     Input : 
         image_paths: a list(N) of string where each string is an image 
         path on the filesystem.
     Output :
-        tiny image features : (N, d) matrix of resized and then vectorized tiny
-        images. E.g. if the images are resized to 16x16, d would equal 256.
+        tiny_images: (N, d) matrix of resized and then vectorized tiny images.
+        E.g. if the images are resized to 16x16, d would equal 256.
     '''
-
     tiny_images = []
-    for i in range (len(image_paths)):
-        img = cv2.imread(image_paths[i])
-        img2D = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        tiny_img2D = cv2.resize(img2D, (16, 16), interpolation=cv2.INTER_LINEAR)
-        tiny_img1D = tiny_img2D.flatten()
-        mean = np.mean(tiny_img1D)
-        std = np.std(tiny_img1D)
-        tiny_cal = (tiny_img1D - mean)/std
-        
-        tiny_images.append(tiny_cal)
-
-    tiny_images = np.matrix(tiny_images)     
-
-    ##############################################################################
-    #                                END OF YOUR CODE                            #
-    ##############################################################################
-
-    return tiny_images
+    for i in range(len(image_paths)):
+        img = cv2.cvtColor(cv2.imread(image_paths[i]), cv2.COLOR_BGR2GRAY)
+        square = crop_square(img)
+        tiny = cv2.resize(square, (16, 16), interpolation=interpolation)
+        tiny1D = tiny.flatten()
+        norm = (tiny1D - np.mean(tiny1D)) / np.std(tiny1D)
+        tiny_images.append(norm)
+    ###########################################################################
+    #                                END OF YOUR CODE                         #
+    ###########################################################################
+    return np.matrix(tiny_images)
