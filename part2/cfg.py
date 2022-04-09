5d4dd7a0b4989380b57e3919295c71052bc66a0e
Icymaple16 <74100443+Icymaple16@users.noreply.github.com>
2022-04-10 00:09:35 +0800

    Update Cfg.py params

--- part2/cfg.py
+++ part2/cfg.py
@@ -44,7 +44,7 @@ TrainingModel_cfg = {
     'data_root' : './p2_data/annotations/train_annos.json',
     
     # ratio of training images and validation images 
-    'split_ratio': 0.01,
+    'split_ratio': 0.9,
     # set a random seed to get a fixed initialization 
     'seed': 687,
     
@@ -53,6 +53,6 @@ TrainingModel_cfg = {
     'lr':0.01,
     'milestones': [15, 25],
     'num_out': 10,
-    'num_epoch': 1,
+    'num_epoch': 25,
     
 }
